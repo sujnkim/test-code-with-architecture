d5c5a5a10054d310178bd8b3a84af691250aed8a
sujnkim <blackout3210@naver.com>
2024-07-06 12:48:51 +0900

    test: userController 테스트코드 추가
    인증코드가 일치하지 않을 경우

--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -72,6 +72,16 @@ public class UserControllerTest {
     }
 
     @Test
+    void 사용자는_인증코드가_일치하지_않을_경우_권한없음_에러를_내려준다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(get("/api/users/2/verify")
+                        .queryParam("certificationCode", "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaac"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
     void 사용자는_내_정보를_불러올_때_개인정보인_주소도_가져올_수_있다() throws Exception {
         // given
         // when
